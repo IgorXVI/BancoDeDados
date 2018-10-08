f64ecb06ec3a184d7c947886e5de6727499713fd
Alunos <inazumaseleven04@gmail.com>
2018-10-08 20:55:35 -0300

    qqqqq

---
+++ app/src/main/java/uricer/edu/br/bancodedados/AdicionarAlunoActivity.kt
@@ -0,0 +1,43 @@
+package uricer.edu.br.bancodedados
+
+import android.content.Context
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.EditText
+
+class AdicionarAlunoActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_adicionar_aluno)
+    }
+
+    fun salvar (botao : View) {
+        var etNome = findViewById<EditText>(R.id.nome)
+        var etCurso = findViewById<EditText>(R.id.curso)
+        var etPont = findViewById<EditText>(R.id.pont)
+
+        val nome = etNome.text.toString()
+        val curso = etCurso.text.toString()
+        val pont = etPont.text.toString().toInt()
+
+        var banco = openOrCreateDatabase(
+                "AlunoDB1",
+                Context.MODE_PRIVATE,
+                null
+        )
+
+        banco.execSQL("""
+            INSERT INTO aluno ('nome', 'curso', 'pont')
+            VALUES ('$nome', '$curso', '$pont')
+        """)
+
+        banco.close()
+
+        finish()
+
+
+
+    }
+}
---
+++ app/src/main/java/uricer/edu/br/bancodedados/MainActivity.kt
@@ -0,0 +1,106 @@
+package uricer.edu.br.bancodedados
+
+import android.content.Context
+import android.content.Intent
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.view.Gravity
+import android.view.View
+import android.widget.TableLayout
+import android.widget.TableRow
+import android.widget.TextView
+import kotlinx.android.synthetic.main.activity_adicionar_aluno.*
+import org.w3c.dom.Text
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        var banco = openOrCreateDatabase(
+                "AlunoDB1",
+                Context.MODE_PRIVATE,
+                null
+        )
+
+        banco.execSQL(
+            """CREATE TABLE IF NOT EXISTS aluno
+                 (
+                    id  INTEGER PRIMARY KEY AUTOINCREMENT,
+                    nome VARCHAR(254),
+                    curso VARCHAR(254),
+                    pont INTEGER
+                 );
+
+                 """
+        )
+
+        banco.close()
+
+    }
+
+
+    fun criarUsuario(view: View) {
+        val intent = Intent(
+                this,
+                AdicionarAlunoActivity::class.java
+        )
+
+        startActivity(intent)
+
+    }
+
+
+    override fun onResume() {
+        super.onResume()
+
+        var banco = openOrCreateDatabase(
+                "AlunoDB1",
+                Context.MODE_PRIVATE,
+                null
+
+        )
+
+        var alunos =
+                findViewById<TableLayout>(R.id.tabela)
+
+        alunos.removeAllViews()
+
+        var linhas = banco.query("aluno", null, null, null, null, null, "pont DESC")
+
+        while (linhas.moveToNext()) {
+            var linha = TableRow(this)
+
+            val id = TextView(this)
+            val nome = TextView(this)
+            val curso = TextView(this)
+            val pont = TextView(this)
+
+            id.gravity = Gravity.CENTER
+            nome.gravity = Gravity.CENTER
+            curso.gravity = Gravity.CENTER
+            pont.gravity = Gravity.CENTER
+
+            id.text = linhas.getString(0)
+            nome.text = linhas.getString(1)
+            curso.text = linhas.getString(2)
+            pont.text = linhas.getString(3)
+
+            linha.addView(id)
+            linha.addView(nome)
+            linha.addView(curso)
+            linha.addView(pont)
+
+            alunos.addView(linha)
+
+        }
+        banco.close()
+
+
+
+
+    }
+
+
+}
